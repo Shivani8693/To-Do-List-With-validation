1a0b360d7ca7d2b47788ed048f5ddc409db8edef
Shivani Gupta <87440737+Shivani8693@users.noreply.github.com>
2023-06-18 11:15:00 +0530

    script.js

--- TO DO LAST TASK/script.js
+++ TO DO LAST TASK/script.js
@@ -1,115 +1,82 @@
 let TableData = [];
 let editIndex = null;
 
-
 function Add() {
+
     var activity = document.getElementById("activity").value;
     var startDate = document.getElementById("start_date").value;
     var dueDate = document.getElementById("due_date").value;
     var status = document.getElementById("Status").value;
-      
-    if(editIndex==null){
-        console.log(editIndex)
-        if (dueDate < startDate) {
-            document.getElementById("text2").style.visibility = "visible";
-            return
-        }    
 
-        if( activity !== "" && startDate !== "" && dueDate !== "" && status !== ""){
+    if (editIndex === null) {
+        if ((activity !== "") && (activity !== "task1") && startDate !== "" && dueDate !== "" && status !== "") {
+            if (dueDate < startDate) {
+                document.getElementById("text2").style.visibility = "visible";
+                return
+
+            }
             var tableList = {
                 Activity: activity,
                 StartDate: startDate,
                 DueDate: dueDate,
                 Status: status,
             };
-
             TableData.push(tableList);
-            tasktable();    
+            tasktable();
             Reset()
-       
-    }else{
-        document.getElementById("text1").style.visibility = "visible";
-        return
-    }
-   
-}else{
-        onEdit(editIndex);
-        
-    }
-    
+        } else {
+            document.getElementById("text1").style.visibility = "visible";
+            return
+        }
+    } 
+    Reset()
+
 }
 
-
 function onEdit(i) {
-   
     var button = document.getElementById("add");
-    button.innerHTML = "Update";
-console.log(i)
+    button.innerHTML = "&#10004Update";
 
-    button.setAttribute("data-index", i);
     var row = document.getElementById("taskrow").children[i];
     var activity = row.cells[0].textContent;
-    console.log(activity)
     var startDate = row.cells[1].textContent;
     var dueDate = row.cells[2].textContent;
     var status = row.cells[3].textContent;
 
-document.getElementById("activity").value = activity;
-console.log(document.getElementById("activity").value)
-document.getElementById("start_date").value = startDate;
-document.getElementById("due_date").value = dueDate;
-document.getElementById("Status").value = status;
+    document.getElementById("activity").value = activity;
+    document.getElementById("start_date").value = startDate;
+    document.getElementById("due_date").value = dueDate;
+    document.getElementById("Status").value = status;
 
-editIndex = i; // Set the edit index to the current row index
-console.log(i)
-console.log('null')
- 
-    if (activity !== "" && (startDate !== "")&&(dueDate !== "") && status !== "") {
-        if(i!==null){
-            button.onclick = function() {
-                updateTodoItem(editIndex);   
-                button.innerHTML = `<i class="bi-plus-lg"></i>add`// Change back to "Update" after editing;  
-            }
+    editIndex = i; // Set thedit index to the current row index
+    button.onclick = function () {
+        var activity = document.getElementById("activity").value;
+        var startDate = document.getElementById("start_date").value;
+        var dueDate = document.getElementById("due_date").value;
+        var status = document.getElementById("Status").value;
+
+        if (i == null) {
+            Add()
+        } else {
+            TableData[i].Activity = activity;
+            TableData[i].StartDate = startDate;
+            TableData[i].DueDate = dueDate;
+            TableData[i].Status = status;
+            tasktable();
+            Reset();
+            button.innerHTML = `<i class="bi-plus-lg"></i>add`// Change back to "Add after editing;
+
+            i = null;
         }
-        
-    }else{
-            document.getElementById("text1").style.visibility = "visible";
-            
-        }  
-        
- }
 
+    }
 
-
-function updateTodoItem(i) {
-
-var activity = document.getElementById("activity").value;
-var startDate = document.getElementById("start_date").value;
-var dueDate = document.getElementById("due_date").value;
-var status = document.getElementById("Status").value;
-
-TableData[i].Activity = activity;
-TableData[i].StartDate = startDate;
-TableData[i].DueDate = dueDate;
-TableData[i].Status = status;
-
-    tasktable(); 
-    Reset()
-
-i=null
-// console.log(i)
 }
 
 
-function Reset() {
-    document.getElementById("activity").value = "";
-    document.getElementById("start_date").value = "";
-    document.getElementById("due_date").value = "";
-    document.getElementById("Status").value = "";
-    document.getElementById("text2").style.visibility = "hidden";
-    document.getElementById("text1").style.visibility = "hidden";
-    editIndex= null;
-}
+
+
+
 
 
 function tasktable() {
@@ -117,38 +84,30 @@ function tasktable() {
     var tableBody = document.getElementById("taskrow");
     tableBody.innerHTML = "";
 
-    TableData.forEach(function (item,i) {
+    TableData.forEach(function (item, i) {
         var row = document.createElement("tr");
         var activityCell = document.createElement("td");
         activityCell.textContent = item.Activity;
         var startDateCell = document.createElement("td");
         startDateCell.textContent = item.StartDate;
         var dueDateCell = document.createElement("td");
-        dueDateCell.textContent = item.DueDate;
+        dueDateCell.textContent = item.DueDate
         var statusCell = document.createElement("td");
         statusCell.textContent = item.Status;
         var optionCell = document.createElement("td");
 
         optionCell.innerHTML =
-        `<button onClick='onEdit(${i})' class="edit" >Edit</button><button onClick='onDelete(${i})' class ='delete'>Delete</button>`;
+
+            `<button onClick='onEdit(${i})' class="edit" >Edit</button><button onClick='onDelete(${i})' class ='delete'>Delete</button>`;
 
         const currentDate = new Date();
         const dueDate = new Date(item.DueDate);
 
-     var a=document.getElementById('ns')
-     var b=document.getElementById('s')
-     var c=document.getElementById('ip')
-        
-        if((statusCell.textContent==="Completed")||(dueDate < currentDate)&&((statusCell.textContent==="Completed"))){
-                row.style.backgroundColor="lightgreen";
-            }else if((dueDate < currentDate)||((statusCell.textContent==="Due Passed"))){
-                a.disabled=true
-                b.Disabled=true
-                c.Disabled=true
-                row.style.textDecoration = "line-through";
-
-            }
-
+        if ((statusCell.textContent === "Completed") || (dueDate < currentDate) && ((statusCell.textContent === "Completed"))) {
+            row.style.backgroundColor = "lightgreen";
+        } else if ((dueDate < currentDate) || ((statusCell.textContent === "Due Passed"))) {
+            row.style.textDecoration = "line-through";
+        }
         row.appendChild(activityCell);
         row.appendChild(startDateCell);
         row.appendChild(dueDateCell);
@@ -156,20 +115,34 @@ function tasktable() {
         row.appendChild(optionCell);
 
         tableBody.appendChild(row);
-
     });
+
+
 
 }
 
 function onDelete(index) {
-
     TableData.splice(index, 1);
     tasktable();
 
 }
 
 
+function Reset() {
+    editIndex = null;
+    i = null;
+    document.getElementById("activity").value = "";
+    document.getElementById("start_date").value = "";
+    document.getElementById("due_date").value = "";
+    document.getElementById("Status").value = "";
+    document.getElementById("text2").style.visibility = "hidden";
+    document.getElementById("text1").style.visibility = "hidden";
+}
+
+
+
 function Search() {
+
     var tableBody = document.getElementById("taskrow");
     tableBody.innerHTML = "";
 
@@ -181,10 +154,10 @@ function Search() {
             item.DueDate.toLowerCase().includes(searchValue) ||
             item.Status.toLowerCase().includes(searchValue)
         );
+
     });
 
     filteredData.forEach(function (item, i) {
-
         var row = document.createElement("tr");
         var activityCell = document.createElement("td");
         activityCell.textContent = item.Activity;
@@ -197,15 +170,29 @@ function Search() {
         var optionCell = document.createElement("td");
 
         optionCell.innerHTML =
-        `<button onClick='onEdit(${i})' class="edit" >Edit</button><button onClick='onDelete(${i})' class ='delete'>Delete</button>`;
 
+            `<button onClick='onEdit(${i})' class="edit" >Edit</button><button onClick='onDelete(${i})' class ='delete'>Delete</button>`;
+
+        const currentDate = new Date();
+        const dueDate = new Date(item.DueDate);
+
+        if ((statusCell.textContent === "Completed") || (dueDate < currentDate) && ((statusCell.textContent === "Completed"))) {
+            row.style.backgroundColor = "lightgreen";
+        } else if ((dueDate < currentDate) || ((statusCell.textContent === "Due Passed"))) {
+            row.style.textDecoration = "line-through";
+        }
         row.appendChild(activityCell);
         row.appendChild(startDateCell);
         row.appendChild(dueDateCell);
         row.appendChild(statusCell);
         row.appendChild(optionCell);
-        tableBody.appendChild(row);
 
+        tableBody.appendChild(row);
     });
 
 }
+
+
+
+
+
